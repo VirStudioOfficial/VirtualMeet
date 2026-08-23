4b4e3c07c50ac87403881e608687b1e2193d0e6f
AkbarJoon <arshiaqwe1234@gmail.com>
2026-08-24 02:43:31 +0330

    Update room.ts

--- types/room.ts
+++ types/room.ts
@@ -1,1 +1,10 @@
+import { User } from "./user";
 
+export interface Room {
+  id: string;
+  hostId: string;
+  participants: User[];
+  createdAt: Date;
+  isLocked?: boolean;
+  password?: string;
+}
