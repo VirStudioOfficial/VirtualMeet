15964101d8bbb52507b7e7a009541a5e0149b3d2
AkbarJoon <arshiaqwe1234@gmail.com>
2026-08-24 01:55:25 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
       <h1 className="text-6xl font-bold mb-4">
@@ -10,11 +16,16 @@ export default function Home() {
       </p>
 
       <div className="flex gap-4">
-        <button className="bg-white text-black px-6 py-3 rounded-xl">
+        <button
+          onClick={() => router.push("/create")}
+          className="bg-white text-black px-6 py-3 rounded-xl font-bold"
+        >
           ساخت جلسه
         </button>
 
-        <button className="border border-white px-6 py-3 rounded-xl">
+        <button
+          className="border border-white px-6 py-3 rounded-xl"
+        >
           ورود به جلسه
         </button>
       </div>
