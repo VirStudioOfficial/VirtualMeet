54c38782402105245acb8b7191cfb38f79713931
AkbarJoon <arshiaqwe1234@gmail.com>
2026-08-24 14:51:49 +0330

    Update user.ts

--- types/user.ts
+++ types/user.ts
@@ -1,9 +1,17 @@
 export interface User {
   id: string;
+
   username: string;
-  email?: string;
+
+  email: string;
+
   avatar?: string;
-  isHost?: boolean;
-  isMuted?: boolean;
-  isCameraOff?: boolean;
+
+  bio?: string;
+
+  isMuted: boolean;
+
+  isCameraOff: boolean;
+
+  createdAt?: string;
 }
